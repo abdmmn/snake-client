d4c60c1478131a96b8cfb1e7e159422918c110dc
abdmmn <abduljmemon@gmail.com>
2023-02-27 18:27:10 +0000

    play.js

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("successfully connnected to the server!")
+    // code that does something when the connection is first established
+  });
+
+  conn.on("data", (theMessage) => {
+    console.log(theMessage);
+    // code that does something when the connection is first established
+  });
+
+  return conn;
+};
